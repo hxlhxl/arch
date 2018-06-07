1fa7c78d1da9470599336f9b74e3e3d5eab1ed14
hxlhxl <huaxiongcool@126.com>
2018-06-08 03:15:25 +0800

    'vector例子'

--- lang/cpp/tutorial/ch07/project10/index.cpp
+++ lang/cpp/tutorial/ch07/project10/index.cpp
@@ -22,11 +22,56 @@ int main() {
 
     cout << "Enter 17 integers: " << endl;
     inputVector(integer1);
-    // inputVector(integer2);
+    inputVector(integer2);
 
     cout << "After input,the vector contains:\n" << "integer1: " << endl;
     outputVector(integer1);
+    cout << "integer2" << endl;
+    outputVector(integer2);
+
+    cout << "\nEvaluating: integer1 != integer2\n";
+    if (integer1 != integer2) {
+        cout << "integer1 and integer2 are not equal" << endl;
+    }
+
+    vector<int> integer3(integer1);
+    cout << "\nSize of integer3 is: " << integer3.size() << "\nVector after initializtion:\n" << endl;
+    outputVector(integer3);
+    // 赋值
+    cout << "\nAssigning integer2 to integer1\n";
+    integer2 = integer1;
+    cout << "\n integer1 is: \n" << endl;
+    outputVector(integer1);
+    cout << "\n integer2 is: \n" << endl;
+    outputVector(integer2);
+    cout << "\nEvaluating: integer1 == integer2\n";
+    if (integer1 == integer2) {
+        cout << "integer1 and integer2 are equal" << endl;
+    }
+
+    cout << "element at 5 in vector integer1" << endl;
+    cout << integer1[5] << endl;
+
+    // 赋值
+    integer1[5] = 999;
+    cout << "element at 5 in vector integer1" << endl;
+    cout << integer1[5] << endl;
+
+
+    try {
+        cout << "try access elem in integer1 at index 15" << endl;
+        cout << integer1.at(15) << endl;
+    } catch(out_of_range &ex) {
+        cerr << "An exception occurred: \n" << ex.what() << endl;
+    }
+
+    cout << "使用push_back动态改变vector大小" << endl;
+    integer3.push_back(11111);
+    outputVector(integer3);
+
     cout << "-----------" << endl;
+    getchar();
+    getchar();
     return 0;
 }
 
