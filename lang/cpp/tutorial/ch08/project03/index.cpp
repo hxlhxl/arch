109dc1bbd8c30bda444ee9cdd43742b3837aaf22
hxlhxl <huaxiongcool@126.com>
2018-06-11 04:09:51 +0800

    '数组作为函数参数并使用sizeof运算符'

--- lang/cpp/tutorial/ch08/project03/index.cpp
+++ lang/cpp/tutorial/ch08/project03/index.cpp
@@ -13,7 +13,9 @@ void printDoubleArray(int *[2], int size) { {
 
 
 }
-
+void testArraySize(int arr[] ) {
+    cout << "sizeof function param array is: " << sizeof(arr) << endl;
+}
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
     printArray(arr, 5);
@@ -32,5 +34,8 @@ int main() {
 
     int dArr[5][2] = {{1,2}, {3,4},{5,6},{7,8},{9,10}};
     cout << "sizeof dArr is: " << sizeof(dArr) << endl;
+    cout << "sizeof(dArr) / sizeof(dArr[0]) is: " << sizeof(dArr) / sizeof(dArr[0]) << endl;
+
+    testArraySize(arr);
     return 0;
 }   
